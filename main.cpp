be92137478f7205270173ac4399a00f4603c599c
Steven Brescia <steven.brescia@heig-vd.ch>
2024-10-06 00:18:49 +0200

    Update du main.cpp (il m'a mis une version qui n'existe plus sur mon pc)

--- main.cpp
+++ main.cpp
@@ -1,46 +1,62 @@
 /*
 *
-*file main.cpp
-* version 1.0.0
-* author Steven Brescia
-* date : 04.10.2024
-* brief : programme pour trouver tout les nombre premiers entre 2 et la valeire saisie.
-*/
+ *file main.cpp
+ * version 1.0.0
+ * author Steven Brescia
+ * date : 05.10.2024
+ * brief : programme pour trouver tout les nombres premiers entre 2 et la valeure saisie.
+ */
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int valeur, valeur_premier;
+int main() {
+    // initialisation des valeurs
+    int valeur, colone = 0;
     char choix_recommencer;
 
-    cout << "Ce programme affiche tout les nombres premiers entre 2 et votre choix." << endl;
+    cout << "Ce programme affiche tout les nombres premiers entre 2 et un nombre de votre choix." << endl;
 
-do {
+    //demande d'input à l'utilisateur
     do {
-        cout << "Veuillez saisir une valeure comprise entre 2 et 1000" << endl;
-        cin >> valeur;
-    }while (valeur > 1000 || valeur < 2);
+        do {
+            cout << "Veuillez saisir une valeure comprise entre 2 et 1000 :" << endl;
+            cin >> valeur;
+        } while (valeur > 1000 || valeur < 2 || valeur == 1000);
 
-        for (int i = 2; i <= valeur; i++){
-            cout << endl;
-            for (int j = 2; j <= j ; j++){
+        cout << "voici la liste des nombres premiers :" << endl;
+        // boucle pour compter le nombre de diviseurs
+        for (int dividende = 2; dividende <= valeur; dividende++) {
+            int nb_diviseur = 0; // variable qui va compter le nombre de diviseurs.
 
-            valeur_premier = i % j;
+            for (int diviseur = 1; diviseur <= dividende; diviseur++) {
+                if (dividende % diviseur == 0) {
+                    nb_diviseur++;
+                }
             }
-                for (int k = 0; k < 5; k++) {
-                    if (valeur_premier == 0){
-                    cout << valeur_premier;
-                    }
 
+            //conditions sur le nombre de diviseurs et affichage des nombres premier.
+            if (nb_diviseur == 2) {
+                cout << '\t' << dividende << " ";
+                if (colone < 4) {
+                    colone++;
+                } else {
+                    cout << endl;
+                    colone = 0;
+                }
+            }
         }
-    }
-cout << endl;
-    do{
-        cout << "voulez-vous recommencer [O/N] ?" << endl;
-        cin >> choix_recommencer;
-    } while (choix_recommencer != 'O' && choix_recommencer != 'N');
-}while ( choix_recommencer != 'N');
+        cout << endl;
+        cout << endl;
+
+        //demande pour recommencer le programme
+        do {
+            cout << "voulez-vous recommencer [O/N] ?" << endl;
+            cin >> choix_recommencer;
+        } while (choix_recommencer != 'O' && choix_recommencer != 'N' &&
+          choix_recommencer != 'o' && choix_recommencer != 'n');
+    } while (choix_recommencer != 'N' && choix_recommencer != 'n');
+
+    cout << "Fin du programme.";
     return EXIT_SUCCESS;
 }
